6ede4b86e0cb0211c9fe7f6604c66d5ea3b4c7ee
Sreedevi0602 <sreedevivs1002@gmail.com>
2024-12-29 04:43:20 +0530

    added new 3 services, added pricing title, initiated pricing component

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
       <About/>
       <Title title='Services'/>
       <Services/>
+      <Title title='Pricing'/>
       
     </div>
   )
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -2,33 +2,63 @@ import React from 'react'
 import './Services.css'
 import consulting7 from '../../assets/consulting7.png'
 import tech2 from '../../assets/tech2.png'
+import branding1 from '../../assets/branding1.png'
+import design2 from '../../assets/design2.png'
+import compliance2 from '../../assets/compliance2.png'
 import training2 from '../../assets/training2.png'
 
 const Services = () => {
   return (
-    
-    <div className='services'>
-      <div className="service">
+    <div>
+    <div className='services1'>
+      <div className="service1">
           <img src={consulting7} alt=''/>
-          <div className='caption'>
+          <div className='caption1'>
             <p><strong>Strategic Consulting</strong></p>
           </div>
       </div>
 
-      <div className="service">
+      <div className="service1">
         <img src={tech2} alt=''/>
-        <div className='caption'>
+        <div className='caption1'>
             <p><strong>Tech Support & Development</strong></p>
           </div>
       </div>
 
-      <div className="service">
-        <img src={training2} alt=''/>
-        <div className='caption'>
+      <div className="service1">
+        <img src={branding1} alt=''/>
+        <div className='caption1'>
             <p><strong>Digital Marketing & Branding</strong></p>
           </div>
       </div>
+      </div>
+    
 
+    <div className='services2'>
+    <div className="service2">
+    <img src={design2} alt=''/>
+          <div className='caption2'>
+            <p><strong>Design & Creative Strategy</strong></p>
+    </div>
+    </div>
+
+    <div className="service2">
+    <img src={compliance2} alt=''/>
+          <div className='caption2'>
+            <p><strong>Business Compliance</strong></p>
+    </div>
+    </div>
+
+    <div className="service2">
+    <img src={training2} alt=''/>
+          <div className='caption2'>
+            <p><strong>Training & Development</strong></p>
+    </div>
+    </div>
+
+    
+
+    </div>  
     </div>
   )
 }
